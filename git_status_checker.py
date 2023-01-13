a53524690a0a2cbe2e3c179f721c063edbc8bef1
alexfung <alexfungshunhang@gmail.com>
2023-01-12 22:03:22 -0800

    Feature: active branch checker

---
+++ git_status_checker.py
@@ -0,0 +1,10 @@
+from git import Repo
+import os
+
+
+class GitStatusChecker:
+    def __init__(self, git_dir):
+        self.repo = Repo(git_dir)
+
+    def get_active_branch(self):
+        return str(self.repo.head.ref)
